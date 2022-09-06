9ae67370a7c4fd9bde3acd65915241a67bf83621
faizal <hajjamalfaizal1998@gmail.com>
2022-09-06 14:18:13 +0530

    logo_added

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import './Header.css'
+import logo from './Images/circuit.png'
 const Header = ({title}) => {
 
         
   return (
     <header className="Header">
       <h1>{title}</h1>
+      <img src={logo} alt="logo"/>
     </header>
   )
 }
